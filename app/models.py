a9e509f44e7643f3936d65315e6d04a74de6c395
Valentin Schabschneider <valentin.schabschneider@gmail.com>
2025-02-25 08:26:19 +0100

    Update models.py (#10)

--- app/models.py
+++ app/models.py
@@ -11,13 +11,7 @@ class LogbookBase(SQLModel):
 class Logbook(LogbookBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
 
-    # record_states: list["RecordState"] = Relationship(
-    #     back_populates="logbook", cascade_delete=True
-    # )
-
-
 class RecordStateBase(SQLModel):
-    # logbook_id = Column(UUID(as_uuid=True), ForeignKey("logbook.id"))
     data: dict | list = Field(sa_type=JSON)
     meta: dict = Field(default={}, sa_type=JSON)
 
@@ -30,14 +24,14 @@ class RecordStateCreate(RecordStateBase):
 
 
 class RecordState(RecordStateBase, table=True):
+    __tablename__: str = "record_state"
+    
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     key: str = Field(max_length=255)
     logbook_id: uuid.UUID = Field(foreign_key="logbook.id")
     created_at: datetime = Field(
         default_factory=datetime.now,
     )
-
-    # logbook: Logbook = Relationship(back_populates="record_states")
 
     __table_args__ = (
         UniqueConstraint("logbook_id", "key", "created_at", name="uq_record_state"),
